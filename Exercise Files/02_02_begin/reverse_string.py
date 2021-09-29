c3daa92918cfeed3c438a5ce1743f882a23211d3
bit-jax <axel.omley@gmail.com>
2021-09-28 20:16:22 -0700

    first

---
+++ Exercise Files/02_02_begin/reverse_string.py
@@ -0,0 +1,21 @@
+"""
+Python Data Structures - A Game-Based Approach
+Stack challenge
+Robin Andrews - https://compucademy.net/
+"""
+
+import stack
+
+string = "gninraeL nIdekniL htiw tol a nraeL"
+reversed_string = ""
+s = stack.Stack()
+
+for i in string:
+    s.push(i)
+while s.size():
+    reversed_string += s.pop()
+print(reversed_string)
+
+
+
+print(reversed_string)
---
+++ Exercise Files/02_02_begin/stack.py
@@ -0,0 +1,42 @@
+"""
+Python Data Structures - A Game-Based Approach
+Stack class
+Robin Andrews - https://compucademy.net/
+"""
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return not self.items
+
+    def push(self,item):
+        return self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+
+if __name__ == '__main__':
+        s = Stack()
+        print(s)
+        print(s.is_empty())
+        s.push(3)
+        print(s)
+        s.push(5)
+        s.push(7)
+        print(s)
+        print(s.pop())
+        print(s)
+        print(s.peek())
+        print(s.size())
